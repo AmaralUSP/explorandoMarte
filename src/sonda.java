1b3c2ebacfb1d315f7d1b6ef70f9b4fb87a543cf
Wellington <amaral_wellington@hotmail.com>
2021-11-03 18:35:12 -0300

    definicoes iniciais do objeto movel controlado pelo usuario

---
+++ src/sonda.java
@@ -0,0 +1,88 @@
+package src;
+
+public class sonda extends objeto{
+    private posicao finalPos;
+    private int currDirection;
+
+    private int[][] moviments = {{ 0, 1},
+                                 { 1, 0},
+                                 { 0,-1},
+                                 {-1, 0}};
+
+    public sonda(int x, int y, char initialDirection){
+        super(x, y);
+        this.finalPos = new posicao(x, y);
+        this.currDirection = 0;
+        this.currDirection = directionToInt(initialDirection);
+    }
+    public posicao getfinalPos(){
+        return this.finalPos;
+    }
+    public int getcurrDirection(){
+        return this.currDirection;
+    }
+    private int directionToInt(char direction){
+        switch (direction){
+            case 'N':
+               return 0;
+            case 'E':
+                return 1;
+            case 'S':
+                return 2;
+            case 'W':
+                return 3;
+            default:
+                System.out.println("Direcao invalida");
+                return 0;
+        }
+    }
+    public char intToDirection(int direction){
+        switch (direction){
+            case 0:
+               return 'N';
+            case 1:
+                return 'E';
+            case 2:
+                return 'S';
+            case 3:
+                return 'W';
+            default:
+                System.out.println("Direcao invalida");
+                return '0';
+        }
+    }
+    public int pousar(String instructions){
+        if(instructions.length() == 0){
+            return 0;
+        }else{
+            int numInstructions = instructions.length();
+            for(int index=0; index<numInstructions; index++){
+                char currInstruction = instructions.charAt(index);
+                // System.out.println("x: " + this.finalPos.getxPosition() + " y: " + this.finalPos.getyPosition() + ' ' + this.getcurrDirection());
+                switch (currInstruction){
+                    case 'L':
+                        if(this.currDirection-1 < 0){
+                            this.currDirection = 3;
+                        } else{
+                            this.currDirection = this.currDirection-1;
+                        }
+                    break;
+                    case 'R':
+                        if(this.currDirection+1 > 3){
+                            this.currDirection = 0;
+                        } else{
+                            this.currDirection = this.currDirection+1;
+                        }
+                    break;
+                    case 'M':
+                        this.finalPos.setxPosition(this.finalPos.getxPosition() + moviments[this.currDirection][0]);
+                        this.finalPos.setyPosition(this.finalPos.getyPosition() + moviments[this.currDirection][1]);
+                    break;
+                    default:
+                        System.out.println("Comando invalido");
+                }
+            }
+            return 1;
+        }
+    }
+}
