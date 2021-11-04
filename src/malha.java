49ff81a1059d3c11a8117139aafaf17f5d716ab8
Wellington <amaral_wellington@hotmail.com>
2021-11-04 14:23:46 -0300

    adicionando sonda e obstaculos simultaneamente

--- src/malha.java
+++ src/malha.java
@@ -1,14 +1,14 @@
 package src;
+import java.util.*;
 
 public class malha{
-    private posicao[] shipPos;
+    List<posicao> objPos=new ArrayList<posicao>();  
     private int xSize;
     private int ySize;
 
-    public malha(int x, int y, int numShip){
+    public malha(int x, int y){
         setxSize(x);
         setySize(y);
-        this.shipPos = new posicao[numShip];
     }
     public void setxSize(int x){
         this.xSize = x;
@@ -22,5 +22,15 @@ public class malha{
     public int getySize(){
         return this.ySize;
     }
-
+    public void printObjectList(){
+        for(posicao pos : this.objPos)
+            System.out.println("x: " + pos.getxPosition() + " y: " + pos.getyPosition() + '\n');
+    }
+    public boolean addNewObject(posicao newObject){
+        for(posicao pos : this.objPos){
+            if(pos.getxPosition()==newObject.getxPosition() && pos.getyPosition()==newObject.getyPosition())
+                return false;
+        }
+        return this.objPos.add(newObject);
+    }
 }
